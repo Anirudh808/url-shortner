cb000317ec82cf996b1a1889242f2fee87797657
anirudhmounasamy <anirudhmounasamy@gmail.com>
2024-11-26 12:29:41 +0530

    updated frontend url

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -15,9 +15,7 @@ export function Main() {
       }
     );
     console.log(data);
-    setShortUrl(
-      `https://url-shortner-app-chi.vercel.app/${data.data.shortUrl}`
-    );
+    setShortUrl(`https://shorturl-pi.vercel.app//${data.data.shortUrl}`);
   }
 
   const handleCopy = () => {
